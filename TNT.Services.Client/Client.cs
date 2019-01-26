df6a7b3ee946b70e92860d80b942c0dd37b214e6
Steve <stevetripp@comcast.net>
2019-01-25 18:13:53 -0700

    Add TNT.Services.Client

---
+++ TNT.Services.Client/Client.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Diagnostics;
+using System.IO;
+using TNT.Services.Models;
+
+namespace TNT.Services.Client
+{
+	public class Client
+	{
+		const string UPLOAD_ENDPOINT = "/Upload/";
+
+		protected IRestClient _RestClient;
+
+		public Client(Uri apiUri)
+		{
+			_RestClient = new RestClient(apiUri);
+		}
+
+		public Response Upload(int applicationId, string fileName)
+		{
+			Response response = null;
+
+			try
+			{
+				var fileBytes = File.ReadAllBytes(fileName);
+
+				var releaseRequest = new ReleaseRequest()
+				{
+					ApplicationID = applicationId,
+					Version = GetVersion(fileName),
+					Base64EncodedFile = Convert.ToBase64String(fileBytes)
+				};
+
+				var restRequest = new RestRequest(UPLOAD_ENDPOINT, Method.POST, DataFormat.Json).AddJsonBody(releaseRequest);
+				var restResponse = _RestClient.Execute(restRequest);
+
+				if (restResponse.IsSuccessful)
+				{
+					response = JsonConvert.DeserializeObject<Response>(restResponse.Content);
+				}
+				else
+				{
+					response = new Response(restResponse.ErrorException);
+				}
+			}
+			catch (Exception error)
+			{
+				response = new Response(error);
+			}
+
+			return response;
+		}
+
+		private static string GetVersion(string fileName)
+		{
+			var fvi = FileVersionInfo.GetVersionInfo(fileName);
+			return fvi.FileVersion;
+		}
+	}
+}
--- TNT.Update.Utility/Program.cs
+++ TNT.Update.Utility/Program.cs
@@ -1,9 +1,7 @@
-﻿using Newtonsoft.Json;
-using RestSharp;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
-using TNT.Services.Models;
+using TNT.Services.Client;
 
 namespace TNT.Upload.Utility
 {
@@ -12,73 +10,22 @@ namespace TNT.Upload.Utility
 		static void Main(string[] args)
 		{
 			var parameters = new Parameters();
+			var client = new Client(new Uri("https://localhost:44328/api/v1"));
 
 			if (!parameters.ParseArgs(args)) return;
-			System.Diagnostics.Debugger.Launch();
 
 			var fi = new FileInfo(parameters.FilePath);
-
 			var fvi = FileVersionInfo.GetVersionInfo(fi.FullName);
-			//var ass = Assembly.LoadFile(fi.FullName);
 
+			var response = client.Upload(parameters.ApplicationId, fvi.FileName);
 
-
-			//var fileVersion = Utilities.Utilities.GetAssemblyAttribute<AssemblyFileVersionAttribute>(ass);
-			//var infoVersion =Utilities.Utilities.GetAssemblyAttribute<AssemblyInformationalVersionAttribute>(ass);
-			//var version = Utilities.Utilities.GetAssemblyAttribute<AssemblyVersionAttribute>(ass);
-
-			//System.Diagnostics.Debugger.Launch();
-			var relReq = new ReleaseRequest();
-
-			relReq.ApplicationID = parameters.ApplicationId;
-			relReq.Version = fvi.FileVersion;
-			string fileName = Path.GetFileName(fvi.FileName);
-
-			var client = new RestClient("https://localhost:44328/api/v1");
-
-			var request = new RestRequest("/Upload/", Method.POST);
-
-			var bytes = File.ReadAllBytes(fileName);
-			relReq.Base64EncodedFile = Convert.ToBase64String(bytes);
-
-			// Json to post.
-			string jsonToSend = JsonConvert.SerializeObject(relReq);
-
-			request.AddParameter("application/json; charset=utf-8", jsonToSend, ParameterType.RequestBody);
-			request.RequestFormat = DataFormat.Json;
-
-			try
+			if (response.IsSuccess)
 			{
-				var restResponse = client.Execute(request);
-
-				if (restResponse.IsSuccessful)
-				{
-					var response = JsonConvert.DeserializeObject<Response>(restResponse.Content);
-
-					if (response.IsSuccess)
-					{
-						Console.WriteLine("Application updated successfully");
-					}
-					else
-					{
-						Console.WriteLine(response.Message);
-					}
-				}
-				//client.ExecuteAsync(request, response =>
-				//{
-				//	if (response.StatusCode == HttpStatusCode.OK)
-				//	{
-				//		// OK
-				//	}
-				//	else
-				//	{
-				//		// NOK
-				//	}
-				//});
+				Console.WriteLine("Application updated successfully");
 			}
-			catch (Exception error)
+			else
 			{
-				// Log
+				Console.WriteLine(response.Message);
 			}
 		}
 	}
