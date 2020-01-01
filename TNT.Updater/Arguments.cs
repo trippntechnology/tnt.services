b1a3048bd1c86ba4fe99e66f25371dafbdf61773
Steve <stevetripp@comcast.net>
2019-12-31 17:05:06 -0700

    Stubbed in TNT.Updater

---
+++ TNT.Updater/Arguments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNT.ArgumentParser;
+
+namespace TNT.Updater
+{
+	class Arguments : ArgumentParser.ArgumentParser
+	{
+		private const string APPLICATION = "a";
+
+		public string Application { get { return (this[APPLICATION] as FileArgument).Value; } }
+
+		public Arguments() : base()
+		{
+			Add(new FileArgument(APPLICATION, "Application to update", true, true));
+		}
+	}
+}
---
+++ TNT.Updater/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TNT.Updater
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			var arguments = new Arguments();
+
+			try
+			{
+				var args = Environment.GetCommandLineArgs();
+				arguments.Parse(args.Skip(1).ToArray(), false);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return;
+			}
+			System.Diagnostics.Debugger.Launch();
+
+			Application.Run(new Form1());
+		}
+	}
+}
