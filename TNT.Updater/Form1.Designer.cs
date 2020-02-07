5a550f7b82ee97153ba338d1526120f1f4b330d4
Steve <stevetripp@comcast.net>
2020-02-06 20:35:03 -0700

    Add tnt.ico

--- TNT.Updater/Form1.Designer.cs
+++ TNT.Updater/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.label1 = new System.Windows.Forms.Label();
 			this.labelInstalledVersion = new System.Windows.Forms.Label();
 			this.labelCurrentVersion = new System.Windows.Forms.Label();
@@ -116,6 +117,7 @@
 			this.Controls.Add(this.labelInstalledVersion);
 			this.Controls.Add(this.label1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "Form1";
