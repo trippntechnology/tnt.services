c1238672855c5d69fcc5b988b715e9d5b142b2c3
Steve Tripp <steve@trippntechnology.com>
2025-01-11 12:21:09 -0700

    Add authorize access to Licenses

--- TNT.Services.Service/Controllers/LicenseesController.cs
+++ TNT.Services.Service/Controllers/LicenseesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TNT.Services.Service.Data;
@@ -7,6 +8,7 @@ using TNT.Services.Service.Models.Entities;
 
 namespace TNT.Services.Service.Controllers;
 
+[Authorize]
 public class LicenseesController : Controller
 {
   private readonly ApplicationDbContext _context;
