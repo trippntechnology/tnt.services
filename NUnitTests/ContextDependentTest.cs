01c42cd5d9acd5e491b22aa6d9de3667d99b653e
Steve Tripp <steve@trippntechnology.com>
2025-01-28 09:06:07 -0700

    Add NUnit tests.

---
+++ NUnitTests/ContextDependentTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Text;
+using TNT.Services.Service.Models.Entities;
+
+namespace NUnitTests;
+
+public abstract class ContextDependentTests
+{
+  private List<Application>? _Applications = null;
+  private List<Release>? _Releases = null;
+
+  protected List<Application> Applications
+  {
+    get
+    {
+      if (_Applications == null)
+      {
+        _Applications = new List<Application>
+        {
+          new Application(){ ID = Guid.NewGuid(), Name= "Application1"},
+          new Application(){ ID = Guid.NewGuid(), Name= "Application2"},
+          new Application(){ ID = Guid.NewGuid(), Name= "Application3"},
+        };
+      }
+      return _Applications;
+    }
+  }
+
+  protected List<Release> Releases
+  {
+    get
+    {
+      if (_Releases == null)
+      {
+        _Releases = new List<Release>
+        {
+          new Release() {
+            ApplicationID = Applications.First().ID,
+            Date = DateTime.Now,
+            FileName = "setup.exe",
+            ID = 1,
+            Package = Encoding.ASCII.GetBytes("Package"),
+            Version = "1.2.3.4" }
+        };
+      }
+      return _Releases;
+    }
+  }
+
+  protected DbSet<T> GetDbSet<T>(List<T> results) where T : class
+  {
+    var data = new List<T>(results).AsQueryable();
+    var mockDbSet = new Mock<DbSet<T>>();
+    mockDbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+    mockDbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+    mockDbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+    mockDbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+    return mockDbSet.Object;
+  }
+}
---
+++ NUnitTests/V2ControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using TNT.Services.Service.Controllers;
+using TNT.Services.Service.Data;
+
+namespace NUnitTests;
+
+public class V2ControllerTests
+{
+  //private var baseUri = new Uri("https://localhost:5001/api");
+  private Guid appId = Guid.Parse("fa1f92e1-4beb-4675-9290-0af2265909a2");
+  private string secret = "2gHpq!TYt9xop65hSp";
+  private Guid licenseeId = Guid.Parse("bc49bedc-80f9-4dca-b386-8739147f200d");
+
+  [Test]
+  public void Test()
+  {
+    var mockContext = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
+    var sut = new V2Controller(mockContext.Object);
+    var result = sut.Test();
+    Assert.That(result.IsSuccess, Is.True);
+    Assert.That(result.Data, Is.Not.Null);
+    Assert.That(result.Data, Is.EqualTo("Congratulations! You have successfully connected."));
+  }
+
+  [Test]
+  public void Authenticate() { }
+
+  //[Test]
+  //public void ApplicationInfo()
+  //{
+  //  var mockContext = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
+  //  mockContext.Setup(m => m.Application).Returns(GetDbSet(Applications));
+    
+  //  var sut = new V2Controller(mockContext.Object);
+
+  //  var result = sut.ApplicationInfo(appId);
+  //}
+}
